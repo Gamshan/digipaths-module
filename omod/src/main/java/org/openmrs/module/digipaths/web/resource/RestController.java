85a7f264aca18507905ce8f2d075c921ae7c3a9a
gamshan <gamshan001@gmail.com>
2025-11-12 11:21:07 +0530

    digipath review and fixes based on html5 recomendation

--- omod/src/main/java/org/openmrs/module/digipaths/web/resource/RestController.java
+++ omod/src/main/java/org/openmrs/module/digipaths/web/resource/RestController.java
@@ -1,13 +1,7 @@
 package org.openmrs.module.digipaths.web.resource;
 
-import org.openmrs.Concept;
-import org.openmrs.Obs;
-import org.openmrs.Order;
-import org.openmrs.Patient;
-import org.openmrs.api.ConceptService;
-import org.openmrs.api.ConditionService;
-import org.openmrs.api.OrderService;
-import org.openmrs.api.PatientService;
+import org.openmrs.*;
+import org.openmrs.api.*;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.digipaths.Department;
 import org.openmrs.module.digipaths.api.DepartmentService;
@@ -130,6 +124,10 @@ public class RestController extends MainResourceController {
 	}
 	
 	public void getHemoglobinAction(DepartmentService departmentService, String patientUuid, List<Map<String,Object>> list) {
+
+		if(!(existDiabetes(departmentService, patientUuid) || existType2Diabetes(departmentService,patientUuid)))
+			return;
+
 		String hemoglobinUUID = "159644AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 		Order order = departmentService.getOrderByPatientUuidAndConceptUuid(patientUuid, hemoglobinUUID);
 		Calendar calendar = Calendar.getInstance();
@@ -143,29 +141,44 @@ public class RestController extends MainResourceController {
 	
 	public void getAlbuminRatioAction(DepartmentService departmentService, String patientUuid, List<Map<String, Object>> list) {
 		String albuminUUID = "164948AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-		String TBD2_Name = "Diabetes mellitus, type 2";
 		String ACE_Inhibitor_Name = "ACE inhibitors";
 		
 		ConceptService conceptService = Context.getService(ConceptService.class);
-		Concept concept = conceptService.getConceptByName(TBD2_Name);
 		Concept aceInhibitorConcept = conceptService.getConceptByName(ACE_Inhibitor_Name);
+
+		Context.getOrderService().getAllOrdersByPatient(new Patient(123)).stream().anyMatch(e-> {
+
+			return e.getConcept() != null && e.getConcept().equals(aceInhibitorConcept);
+		});
+
+
 		
-		if (concept != null) {
-			boolean existTBD2 = departmentService.getConditionByPatientUuidAndConceptUuid(patientUuid, concept.getUuid());
-			boolean existAceInhibitor = aceInhibitorConcept != null
-			        && departmentService.getConditionByPatientUuidAndConceptUuid(patientUuid, aceInhibitorConcept.getUuid());
-			
-			if (existTBD2 && !existAceInhibitor) {
-				List<Obs> obsList = departmentService.getObsByPatientUuidAndConceptUuid(patientUuid, albuminUUID, 1);
-				if (obsList != null && !obsList.isEmpty() && obsList.get(0) != null && obsList.get(0).getValueNumeric() > 3) {
-					list.add(getDigipathsRow(
-					    obsList.get(0).getObsDatetime(),
-					    "Elevated ACR detected in T2DM patient without ACEI",
-					    "The patient with Type 2 Diabetes Mellitus has an elevated urine Albumin-Creatinine Ratio (ACR > 3 mg/mmol), suggesting early signs of diabetic kidney disease.\n"
-					            + "Initiating an ACE inhibitor is recommended to reduce progression of renal damage and provide cardiovascular protection."));
-				}
+		boolean existTBD2 = existType2Diabetes(departmentService, patientUuid);
+		boolean existAceInhibitor = aceInhibitorConcept != null
+		        && departmentService.existOrderByPatientUuidAndConceptId(patientUuid, aceInhibitorConcept.getId());
+		
+		if (existTBD2 && !existAceInhibitor) {
+			List<Obs> obsList = departmentService.getObsByPatientUuidAndConceptUuid(patientUuid, albuminUUID, 1);
+			if (obsList != null && !obsList.isEmpty() && obsList.get(0) != null && obsList.get(0).getValueNumeric() > 3) {
+				list.add(getDigipathsRow(
+				    obsList.get(0).getObsDatetime(),
+				    "Elevated ACR detected in T2DM patient without ACEI",
+				    "The patient with Type 2 Diabetes Mellitus has an elevated urine Albumin-Creatinine Ratio (ACR > 3 mg/mmol), suggesting early signs of diabetic kidney disease.\n"
+				            + "Initiating an ACE inhibitor is recommended to reduce progression of renal damage and provide cardiovascular protection."));
 			}
 		}
+	}
+	
+	public boolean existDiabetes(DepartmentService departmentService, String patientUuid) {
+		String diabetesUuid = "119481AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		return departmentService.getConditionByPatientUuidAndConceptUuid(patientUuid, diabetesUuid);
+	}
+	
+	public boolean existType2Diabetes(DepartmentService departmentService, String patientUuid) {
+		String TBD2_Name = "Diabetes mellitus, type 2";
+		ConceptService conceptService = Context.getService(ConceptService.class);
+		Concept concept = conceptService.getConceptByName(TBD2_Name);
+		return concept != null && departmentService.getConditionByPatientUuidAndConceptUuid(patientUuid, concept.getUuid());
 	}
 	
 	public void getSerumCreatinineAction(DepartmentService departmentService, String patientUuid,
@@ -194,24 +207,24 @@ public class RestController extends MainResourceController {
 					Double o1GFR = calculateGFR(patient.getGender(), patient.getAge(), o1.getValueNumeric());
 					Double o2GFR = calculateGFR(patient.getGender(), patient.getAge(), o2.getValueNumeric());
 					Map<String, Object> map = new HashMap<>();
-					String messageLessThan45 = "Reduced kidney function detected (GFR < 45)";
-					String messageLessThan60 = "GFR <60 on two occasions three months apart, suspect Chronic Kidney Disease (CKD).";
-					String recommendationLessThan45 = "<p>If the GFR is below 45 mL/min/1.73m², the severity warrants timely referral to a nephrologist for specialized evaluation and management to slow disease progression and address complications.</p>";
-					String recommendationLessThan60 = "<p>The patient has had two separate serum creatinine tests, spaced at least three months apart, both indicating estimated GFR values below 60 mL/min/1.73m².</br> This persistent reduction in kidney function suggests the presence of Chronic Kidney Disease (CKD), as per clinical guidelines.</p>";
+					String messageLessThan45 = "Reduced kidney function detected (eGFR < 45)";
+					String messageLessThan60 = "eGFR <60 on two occasions three months apart, suspect Chronic Kidney Disease (CKD).";
+					String recommendationLessThan45 = "<p>If the eGFR is below 45 mL/min/1.73m², the severity warrants timely referral to a nephrologist for specialized evaluation and management to slow disease progression and address complications.</p>";
+					String recommendationLessThan60 = "<p>The patient has had two separate serum creatinine tests, spaced at least three months apart, both indicating estimated eGFR values below 60 mL/min/1.73m².</br> This persistent reduction in kidney function suggests the presence of Chronic Kidney Disease (CKD), as per clinical guidelines.</p>";
 
 					if(o1GFR < 45) {
-						list.add(getDigipathsRow(o1.getObsDatetime(), messageLessThan45, "<h6> GFR: " + String.format("%.1f", o1GFR)  + " mL/min/1.73m² </h6>" + recommendationLessThan45));
+						list.add(getDigipathsRow(o1.getObsDatetime(), messageLessThan45, "<h6> eGFR: " + String.format("%.1f", o1GFR)  + " mL/min/1.73m² </h6>" + recommendationLessThan45));
 						 messageAdded = true;
 					} else if(o2GFR < 45) {
-						list.add(getDigipathsRow(o2.getObsDatetime(), messageLessThan45, "<h6> GFR: " + String.format("%.1f", o2GFR) + " mL/min/1.73m² </h6>" + recommendationLessThan45));
+						list.add(getDigipathsRow(o2.getObsDatetime(), messageLessThan45, "<h6> eGFR: " + String.format("%.1f", o2GFR) + " mL/min/1.73m² </h6>" + recommendationLessThan45));
 						messageAdded = true;
 					}
 
 					if(!existCKD && o1GFR < 60){
-						list.add(getDigipathsRow(o1.getObsDatetime(), messageLessThan60, "<h6> GFR: " + String.format("%.1f",  o1GFR) + " mL/min/1.73m² </h6>" + recommendationLessThan60));
+						list.add(getDigipathsRow(o1.getObsDatetime(), messageLessThan60, "<h6> eGFR: " + String.format("%.1f",  o1GFR) + " mL/min/1.73m² </h6>" + recommendationLessThan60));
 						messageAdded = true;
 					} else if(!existCKD && o2GFR < 60) {
-						list.add(getDigipathsRow(o2.getObsDatetime(), messageLessThan60,"<h6> GFR: " + String.format("%.1f", o2GFR) + " mL/min/1.73m² </h6>" +  recommendationLessThan60));
+						list.add(getDigipathsRow(o2.getObsDatetime(), messageLessThan60,"<h6> eGFR: " + String.format("%.1f", o2GFR) + " mL/min/1.73m² </h6>" +  recommendationLessThan60));
 						messageAdded = true;
 					}
 				}
